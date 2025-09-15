37d5657d43b059ced21bf5bff0ee4e98e3fba14d
cuitong <72696179+PlasticTong@users.noreply.github.com>
2025-09-15 19:10:09 +0800

    feat: 增加 resource 与路径的映射关系便于查找 (#90)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("readme.md") as f:
 
 setup(
     name="bk_resource",
-    version="0.4.13",
+    version="0.4.14",
     author="blueking",
     url="https://bk.tencent.com",
     author_email="blueking@tencent.com",
